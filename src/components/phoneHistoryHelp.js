d908a75dde9046615a61137f60957b6cfeb08c79
Keith Morris <kmorris@icct.com>
2016-07-31 14:13:22 -0400

    added phone history help

--- src/components/phoneHistoryHelp.js
+++ src/components/phoneHistoryHelp.js
@@ -4,7 +4,17 @@ export default class PhoneHistoryHelp extends React.Component {
     render() {
         return(
             <div id="phoneHistoryHelp__main">
-            	Here's how to clear your phone's browser history.
+            	<h3 className="text-primary">Deleting Your Phone's History</h3>
+            	<p>Here's how to clear your phone's browser history.</p>
+            	<ol id="phoneHistoryHelp__main__chromeList">
+					<li>Open the Chrome app Chrome app.</li>
+					<li>Tap More More and then Settings.</li>
+					<li>Under "Advanced," tap Privacy and then Clear browsing data.</li>
+					<li>At the top, tap the drop-down under "Clear data from the."</li>
+					<li>Choose a time period, such as past hour or past day. To delete everything, select beginning of time.</li>
+					<li>Select the types of information you want to remove.</li>
+					<li>Tap Clear data.</li>
+				</ol>
             </div>
         );
     }
