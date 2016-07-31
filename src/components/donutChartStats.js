21665cf1297afc3f9753cf5cc1dca7727fac9f6e
Chandra <chandra.asar@gmail.com>
2016-07-31 15:26:33 -0400

    stuff

--- src/components/donutChartStats.js
+++ src/components/donutChartStats.js
@@ -1,42 +1,45 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Chart} from 'react-google-charts'
+// import {Chart} from 'react-google-charts'
 
 export default class DonutChartStats extends React.Component {
 
-    constructor(props) {
-       super(props);
-       this.state = {};
-       this.render = this.render.bind(this);
-    }
+  //   constructor(props) {
+  //      super(props);
+  //      this.state = {};
+  //      this.render = this.render.bind(this);
+  //   }
+  //
+  // componentDidMount() {
+  //   let options = {
+  //       title: 'Human Trafficking Statistics Placeholder',
+  //       // hAxis: {title: 'Types of Trafficking', minValue: 2011, maxValue: 2016},
+  //       // vAxis: {title: 'Cases', minValue: 0, maxValue: 100},
+  //       pieHole: ':0.4'
+  //   };
+  //
+  //   let data = [
+  //       ['Types of Trafficking', 'Percentage'],
+  //       [ "Type 1",    20],
+  //       [ "Type 2",    10],
+  //       [ "Type 3",    30],
+  //       [ "Type 4",    20],
+  //       [ "Type 5",    10],
+  //       [ "Type 6",    10]
+  //   ];
+  //   this.setState({
+  //       'data' : data,
+  //       'options' : options
+  //    });
+  //  }
 
-  componentDidMount() {
-    let options = {
-        title: 'Human Trafficking Statistics Placeholder',
-        // hAxis: {title: 'Types of Trafficking', minValue: 2011, maxValue: 2016},
-        // vAxis: {title: 'Cases', minValue: 0, maxValue: 100},
-        pieHole: ':0.4'
-    };
-
-    let data = [
-        ['Types of Trafficking', 'Percentage'],
-        [ "Type 1",    20],
-        [ "Type 2",    10],
-        [ "Type 3",    30],
-        [ "Type 4",    20],
-        [ "Type 5",    10],
-        [ "Type 6",    10]
-    ];
-    this.setState({
-        'data' : data,
-        'options' : options
-     });
-   }
+  // This would have gone in code-wise if it didn't break all of the mocha tests. So now there's a screenshot of what it looked like as a placeholder
+   //<Chart chartType = "PieChart" data = {this.state.data} options = {this.state.options} graph_id = "ScatterChart"  width={"100%"} height={"400px"}  legend_toggle={true} />
 
   render() {
     return (
-      <div className={"donut-chart-container"}>
-        <Chart chartType = "PieChart" data = {this.state.data} options = {this.state.options} graph_id = "ScatterChart"  width={"100%"} height={"400px"}  legend_toggle={true} />
+      <div className="donut-chart-container">
+        <img src="../images/pieChartStats.png" />
       </div>
     );
   }
--- src/components/scatterChartStats.js
+++ src/components/scatterChartStats.js
@@ -1,44 +1,46 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Chart} from 'react-google-charts'
+// import {Chart} from 'react-google-charts'
 
 export default class ScatterChartStats extends React.Component {
 
-    constructor(props) {
-       super(props);
-       this.state = {};
-       this.render = this.render.bind(this);
-    }
+//     constructor(props) {
+//        super(props);
+//        this.state = {};
+//        this.render = this.render.bind(this);
+//     }
+//
+//   componentDidMount() {
+//     let options = {
+//         title: 'Human Trafficking Statistics Placeholder',
+//         hAxis: {title: 'Year', minValue: 2011, maxValue: 2016},
+//         vAxis: {title: 'Cases', minValue: 0, maxValue: 100},
+//         legend: 'none'
+//     };
+//
+//     let data = [
+//         ['Year', 'Cases'],
+//         [ 2011,      18],
+//         [ 2012,      35],
+//         [ 2013,     44],
+//         [ 2014,      70],
+//         [ 2015,      50],
+//         [ 2016,    60]
+//     ];
+//     this.setState({
+//         'data' : data,
+//         'options' : options
+//      });
+// }
 
-  componentDidMount() {
-    let options = {
-        title: 'Human Trafficking Statistics Placeholder',
-        hAxis: {title: 'Year', minValue: 2011, maxValue: 2016},
-        vAxis: {title: 'Cases', minValue: 0, maxValue: 100},
-        legend: 'none'
-    };
+//This would have gone in if it didn't break all of the mocha tests
+//<Chart chartType = "ScatterChart" data = {this.state.data} options = {this.state.options} graph_id = "ScatterChart"  width={"100%"} height={"400px"}  legend_toggle={true} />
 
-    let data = [
-        ['Year', 'Cases'],
-        [ 2011,      18],
-        [ 2012,      35],
-        [ 2013,     44],
-        [ 2014,      70],
-        [ 2015,      50],
-        [ 2016,    60]
-    ];
-    this.setState({
-        'data' : data,
-        'options' : options
-     });
-
-
-}
 
   render() {
     return (
-      <div className={"scatter-chart-container"}>
-        <Chart chartType = "ScatterChart" data = {this.state.data} options = {this.state.options} graph_id = "ScatterChart"  width={"100%"} height={"400px"}  legend_toggle={true} />
+      <div className="scatter-chart-container">
+          <img src="../images/scatterPlotStats.png" />
       </div>
     );
   }
